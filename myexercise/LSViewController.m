5295f1934a7194998d4bf890576502cac5e7448a
Gregory Jeckell <gregory.jeckell@demandmedia.com>
2014-08-13 15:52:15 -0700

    Initial Commit

---
+++ myexercise/LSViewController.m
@@ -0,0 +1,29 @@
+//
+//  LSViewController.m
+//  myexercise
+//
+//  Created by Greg Jeckell on 8/13/14.
+//  Copyright (c) 2014 livestrong. All rights reserved.
+//
+
+#import "LSViewController.h"
+
+@interface LSViewController ()
+
+@end
+
+@implementation LSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ myexercise/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myexercise
+//
+//  Created by Greg Jeckell on 8/13/14.
+//  Copyright (c) 2014 livestrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LSAppDelegate class]));
+    }
+}
