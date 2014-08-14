74eac30ce4988c2b270c2aec9cff4f4aff3aa783
Gregory Jeckell <gregory.jeckell@demandmedia.com>
2014-08-13 18:52:04 -0700

    Working project links

--- myplate/LSViewController.m
+++ myplate/LSViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+//	[[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
+//    NSLog(@"view did load");
 }
 
 - (void)didReceiveMemoryWarning
